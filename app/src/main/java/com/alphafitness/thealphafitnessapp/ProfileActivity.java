eb61e935db27cef857c8206da4f7a7a707353f37
Vikram Deshmukh <srvikram13@gmail.com>
2016-11-09 00:36:19 -0800

    Some more aesthetic changes. Committing all changes before adding Map.

--- app/src/main/java/com/alphafitness/thealphafitnessapp/ProfileActivity.java
+++ app/src/main/java/com/alphafitness/thealphafitnessapp/ProfileActivity.java
@@ -5,16 +5,32 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private EditText weight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+        Spinner gender = (Spinner)findViewById(R.id.user_gender);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_array, android.R.layout.simple_spinner_item);
+
+        weight = (EditText) findViewById(R.id.user_weight);
+        InputFilterMinMax filter = new InputFilterMinMax("50", "499") {};
+        weight.setFilters(new InputFilter[]{filter});
+        weight.setText("180");
+        //adapter.setDropDownViewResource();
+
+        gender.setAdapter(adapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
